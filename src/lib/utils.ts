979b884bd3fa193c4999936862743782321d6d1f
DOKER <alberto.grajales97@unach.mx>
2025-03-13 23:56:27 -0600

    cambions chicos

---
+++ src/lib/utils.ts
@@ -0,0 +1,7 @@
+// filepath: /home/PROYRCTO_INTEGRADOR/src/lib/utils.ts
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
