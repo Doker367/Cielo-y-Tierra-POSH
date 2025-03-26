fcd47c4a7e17a99e78eb1e4d22760fda4769eae9
DOKER <alberto.grajales97@unach.mx>
2025-03-26 14:28:29 -0600

    Actualiza home.hbs con nuevas secciones y estilos

--- src/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-// filepath: /home/PROYRCTO_INTEGRADOR/src/lib/utils.ts
-import { ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
